9c3b2091372bebd4739a8e413be43f004b423907
zfurtak <zfurtak@student.agh.edu.pl>
2021-10-29 14:58:03 +0200

    Lab3

--- oolab/src/test/java/agh/ics/oop/AnimalTest.java
+++ oolab/src/test/java/agh/ics/oop/AnimalTest.java
@@ -9,48 +9,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AnimalTest {
     Animal Bob = new Animal();
     Animal Mike = new Animal();
-
     @Test
-    void orientTest(){
-        Bob.move(MoveDirection.RIGHT);
-        assertEquals(MapDirection.EAST, Bob.getOrient());
-        Bob.move(MoveDirection.RIGHT);
-        assertEquals(MapDirection.SOUTH, Bob.getOrient());
-        Mike.move(MoveDirection.LEFT);
-        assertEquals(MapDirection.WEST, Mike.getOrient());
-    }
-    @Test
-    void positionTest(){
-        Bob.move(MoveDirection.FORWARD);
-        Bob.move(MoveDirection.FORWARD);
-        assertEquals(2, Bob.getPosition().x);
-        assertEquals(4, Bob.getPosition().y);
-        Mike.move(MoveDirection.RIGHT);
-        Mike.move(MoveDirection.FORWARD);
-        Mike.move(MoveDirection.FORWARD);
-        assertEquals(4, Mike.getPosition().x);
-        assertEquals(2, Mike.getPosition().y);
-    }
-    @Test
-    void edgeTest(){
-        Bob.move(MoveDirection.FORWARD);
-        Bob.move(MoveDirection.FORWARD);
-        Bob.move(MoveDirection.FORWARD);
-        Bob.move(MoveDirection.FORWARD);
-        assertEquals(4, Bob.getPosition().y);
-        Mike.move(MoveDirection.BACKWARD);
-        Mike.move(MoveDirection.BACKWARD);
-        Mike.move(MoveDirection.BACKWARD);
-        Mike.move(MoveDirection.BACKWARD);
-        assertEquals(0, Mike.getPosition().y);
-    }
-    @Test void stringTest(){
+    void everythingTest(){
         String[] lista = new String[] {"f", "f", "r"};
         ArrayList<MoveDirection> kierunki = new ArrayList<MoveDirection>(OptionsParser.parse(lista));
         ArrayList<MoveDirection> testowa = new ArrayList<MoveDirection>();
         testowa.add(MoveDirection.FORWARD);
         testowa.add(MoveDirection.FORWARD);
         testowa.add(MoveDirection.RIGHT);
-        assertEquals(testowa, kierunki);
+        for(MoveDirection arg : kierunki){Bob.move(arg);}
+        assertEquals(2, Bob.getPosition().x);
+        assertEquals(4, Bob.getPosition().y);
+        assertEquals(MapDirection.EAST, Bob.getOrient());
+
+        String[] lista1 = new String[] {"b", "b", "b", "l", "f"};
+        ArrayList<MoveDirection> kierunki1 = new ArrayList<MoveDirection>(OptionsParser.parse(lista1));
+        ArrayList<MoveDirection> expected_dir = new ArrayList<MoveDirection>();
+        expected_dir.add(MoveDirection.BACKWARD);
+        expected_dir.add(MoveDirection.BACKWARD);
+        expected_dir.add(MoveDirection.BACKWARD);
+        expected_dir.add(MoveDirection.LEFT);
+        expected_dir.add(MoveDirection.FORWARD);
+        for(MoveDirection arg : kierunki1){Mike.move(arg);}
+        assertEquals(1, Mike.getPosition().x);
+        assertEquals(0, Mike.getPosition().y);
+        assertEquals(MapDirection.WEST, Mike.getOrient());
+
     }
 }
